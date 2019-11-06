79bf1fa6b928166ec9fd0218239e5756c739e6ed
Greg Silverman <gms@umn.edu>
2019-11-06 15:16:54 -0600

    fixed seed

--- scripts/cnn.py
+++ scripts/cnn.py
@@ -1,4 +1,29 @@
+# Seed value
+# Apparently you may use different seed values at each stage
+seed_value= 0
+
+# 1. Set the `PYTHONHASHSEED` environment variable at a fixed value
 import os
+os.environ['PYTHONHASHSEED']=str(seed_value)
+
+# 2. Set the `python` built-in pseudo-random generator at a fixed value
+import random
+random.seed(seed_value)
+
+# 3. Set the `numpy` pseudo-random generator at a fixed value
+import numpy as np
+np.random.seed(seed_value)
+
+# 4. Set the `tensorflow` pseudo-random generator at a fixed value
+import tensorflow as tf
+tf.compat.v1.set_random_seed(seed_value)
+
+# 5. Configure a new global `tensorflow` session
+from keras import backend as K
+session_conf = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
+sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
+K.set_session(sess)
+
 import click
 from pathlib import Path
 from gensim.models import KeyedVectors
@@ -12,6 +37,7 @@ from keras.preprocessing import sequence
 from keras.callbacks import EarlyStopping
 from keras.preprocessing.text import Tokenizer
 from sklearn.preprocessing import LabelBinarizer, LabelEncoder
+
 
 # Set environment variable in Docker to use correct directory
 # if None specified, then default to local machine
@@ -153,6 +179,7 @@ def get_predictive_model():
 
         history = model.fit(X_train, y_train,
                             epochs=epochs,
+                            shuffle=False,
                             callbacks=[EarlyStopping(monitor='val_loss', verbose=1, patience=4)],
                             verbose=2,
                             validation_data=(X_test, y_test),
